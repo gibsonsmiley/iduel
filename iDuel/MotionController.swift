c547bb9c17bf9f011fb4849664a509300e162e77
Vince <vincent.van.snow@gmail.com>
2016-05-16 13:56:58 -0600

    Initial Commit

---
+++ iDuel/MotionController.swift
@@ -0,0 +1,14 @@
+//
+//  MotionController.swift
+//  iDuel
+//
+//  Created by mac-admin on 5/16/16.
+//  Copyright © 2016 Gibson Smiley. All rights reserved.
+//
+
+import Foundation
+
+class MotionController {
+    
+    
+}
