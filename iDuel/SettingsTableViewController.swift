f9dc104ae0627f3e61b8ec6144a26839a02fad24
Gibson Smiley <gibsonsmiley@gmail.com>
2016-05-19 14:25:28 -0600

    Spring cleaning storyboard

--- iDuel/SettingsTableViewController.swift
+++ iDuel/SettingsTableViewController.swift
@@ -20,23 +20,17 @@ class SettingsTableViewController: UITableViewController {
     
     let defaults = NSUserDefaults.standardUserDefaults()
     
+    // MARK: - View
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Table view data source
-    
- //   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-   //     return 0
-    //}
+    // MARK: - Actions
     
     @IBAction func cancelButtonTapped(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
@@ -46,8 +40,6 @@ class SettingsTableViewController: UITableViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    
-    
     @IBAction func soundEffectSwitched(sender: AnyObject) {
         if soundEffectSwitch.on == true {
             defaults.setBool(true, forKey: "sounds")
@@ -55,11 +47,8 @@ class SettingsTableViewController: UITableViewController {
         } else if soundEffectSwitch.on == false {
             defaults.setBool(false, forKey: "sounds")
             NSUserDefaults.standardUserDefaults().boolForKey("sounds")
-
         }
-        
     }
-    
     
     @IBAction func vibrationSwitched(sender: AnyObject) {
         if vibrationSwitch.on == true {
@@ -69,7 +58,6 @@ class SettingsTableViewController: UITableViewController {
             defaults.setBool(false, forKey: "vibrate")
             NSUserDefaults.standardUserDefaults().boolForKey("vibrate")
         }
-        
     }
     
     @IBAction func leftHandedSwitched(sender: AnyObject) {
@@ -79,65 +67,14 @@ class SettingsTableViewController: UITableViewController {
         } else if leftHandedSwitch.on == false {
             defaults.setBool(false, forKey: "leftHand")
             NSUserDefaults.standardUserDefaults().boolForKey("lefthand")
-            
         }
-        
     }
-    
-    
     
 //    func vibrate() {
 //        if NSUserDefaults.standardUserDefaults().boolForKey("vibrate") == true {
 //            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
 //        }
 //    }
-    
-    
-    
-    /*
-     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-     if editingStyle == .Delete {
-     // Delete the row from the data source
-     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-     } else if editingStyle == .Insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
     
     /*
      // MARK: - Navigation
