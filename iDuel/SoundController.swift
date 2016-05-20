484197677d5df6f0627c6318561edd2117a59e29
Ryan <ryangrrtt228@gmail.com>
2016-05-20 15:42:14 -0600

    Created the soundController and a sound folder that has the sounds/ music. I am still waiting for a email from some guys to see if I can use his music.

--- iDuel/SoundController.swift
+++ iDuel/SoundController.swift
@@ -7,12 +7,86 @@
 //
 
 import Foundation
-import UIKit
+import AudioToolbox
 import AVFoundation
 
+extension SystemSoundID {
+    
+    static func playGunShot1(fileName: String, withExtenstion fileExtenstion: String? = "mp3") {
+        var gun1: SystemSoundID = 0
+        if let gun1SoundUrl = NSBundle.mainBundle().URLForResource("1gunshot", withExtension: "mp3") {
+            AudioServicesCreateSystemSoundID(gun1SoundUrl, &gun1)
+            AudioServicesPlaySystemSound(gun1)
+        }
+    }
+    
+    static func playGunShot2(fileName: String, withExtenstion fileExtenstion: String? = "mp3") {
+        var gun2: SystemSoundID = 0
+        if let gun2SoundUrl = NSBundle.mainBundle().URLForResource("2gunshot", withExtension: "mp3") {
+            AudioServicesCreateSystemSoundID(gun2SoundUrl, &gun2)
+            AudioServicesPlaySystemSound(gun2)
+        }
+        
+    }
+    
+    static func playGunCocking(fileName: String, withExtenstion fileExtenstion: String? = "mp3") {
+        var gunCocking: SystemSoundID = 0
+        if let gunCockingSoundUrl = NSBundle.mainBundle().URLForResource("GunCocking", withExtension: "mp3") {
+            AudioServicesCreateSystemSoundID(gunCockingSoundUrl, &gunCocking)
+            AudioServicesPlaySystemSound(gunCocking)
+        }
+        
+    }
+    
+    // Example on how to call this anywhere in the app. 1. import AudioToolbox 2. Then write this SystemSoundId.playGunShot1("1gunshot")
 
-class SoundController {
-    
-    
-    
 }
+
+    // To play the dueling background music just call " playDuelBackGroundMusic("_______.mp3)" or "playMainThemeBackGroundMusic(bensound-epic.mp3)"
+
+var duelBackGroundMusic = AVAudioPlayer()
+
+func playDuelBackGroundMusic(filename: String){
+    let url = NSBundle.mainBundle().URLForResource("______", withExtension: "mp3")
+    guard let newURL = url else {
+        print("could not find file: \("")")
+        return
+    }
+    do {
+        duelBackGroundMusic = try AVAudioPlayer(contentsOfURL: newURL)
+        duelBackGroundMusic.numberOfLoops = -1
+        duelBackGroundMusic.prepareToPlay()
+        duelBackGroundMusic.play()
+    } catch let error as NSError {
+        print(error.description)
+    }
+}
+
+
+var MainThemeBackGroundMusic = AVAudioPlayer()
+
+func playMainThemeBackGroundMusic(filename: String){
+    let url = NSBundle.mainBundle().URLForResource("bensound-epic", withExtension: "mp3")
+    guard let newURL = url else {
+        print("could not find file: \("bensound-epic")")
+        return
+    }
+    do {
+        duelBackGroundMusic = try AVAudioPlayer(contentsOfURL: newURL)
+        duelBackGroundMusic.numberOfLoops = -1
+        duelBackGroundMusic.prepareToPlay()
+        duelBackGroundMusic.play()
+    } catch let error as NSError {
+        print(error.description)
+    }
+}
+
+
+
+
+
+
+
+
+
+
