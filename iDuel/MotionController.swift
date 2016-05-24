491095e7a0d416e7ae1a0626ca8f2905e92ae9be
Vince <vincent.van.snow@gmail.com>
2016-05-24 11:25:05 -0600

    deleted beginMotionTrackingFunction, deleted motionForDuelFunction, deleted several properties

--- iDuel/MotionController.swift
+++ iDuel/MotionController.swift
@@ -14,86 +14,10 @@ import CoreData
 class MotionController {
     
     static let sharedController = MotionController()
-    
-    static var temporaryMeasurements: CMGyroData?
-    var loweredMesaurements: CMGyroData?
-    
-    // pitch = rotation around the x axis
-    // roll = rotation around the y axis
-    // yaw = rotation around the z axis
-    var PitchRollYawArray: [CMDeviceMotion] = []
-
     var motionManager: CMMotionManager!
     
     init() {
         self.motionManager = CMMotionManager()
-    }
-    
-    
-        
-     func trackMotionForDuel(completion: (currentPosition: Calibration?) -> Void) {
-        if motionManager.deviceMotionAvailable {
-            motionManager.deviceMotionUpdateInterval = 0.1
-            let queue = NSOperationQueue.mainQueue()
-            let motionQueue = dispatch_queue_create("duelTracking", nil)
-            dispatch_async(motionQueue, {
-                self.motionManager.startDeviceMotionUpdatesToQueue(queue, withHandler: { (data, error) in
-                    if let error = error {
-                        print(error.localizedDescription)
-                        completion(currentPosition: nil)
-                    } else {
-                        guard let data = data else { completion(currentPosition: nil); return }
-                        let pitch = data.attitude.pitch
-                        let roll = data.attitude.roll
-                        let yaw = data.attitude.yaw
-                        let currentPosition = Calibration(pitch: pitch, roll: roll, yaw: yaw)
-                        completion(currentPosition: currentPosition)
-                    }
-                })
-            })
-        }
-    }
-    
-   
-    
-     func beginMotionTracking(completion:(averageCalibration: Calibration?) -> Void) {
-
-        if motionManager.deviceMotionAvailable {
-            motionManager.startDeviceMotionUpdates()
-            motionManager.gyroUpdateInterval = 1.0
-            let queue = NSOperationQueue.mainQueue()
-            let motionQueue = dispatch_queue_create("", nil)
-            
-            //gathers CMGyroData every second
-            dispatch_async(motionQueue, {
-                self.motionManager.startDeviceMotionUpdatesToQueue(queue, withHandler: { (data, error) in
-                    if error != nil {
-                        print(error?.localizedDescription)
-                        completion(averageCalibration: nil)
-                    } else {
-                        guard let data = data else { completion(averageCalibration: nil); return }
-                        self.PitchRollYawArray.append(data)
-                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-                    }
-                })
-            })
-            
-            //set up background queue to count to 5, stops observing gyro data after 5 seconds
-            let backgroundQueue = dispatch_queue_create("", nil)
-            dispatch_async(backgroundQueue, {
-                NSThread.sleepForTimeInterval(5.0)
-                self.motionManager.stopDeviceMotionUpdates()
-                print(self.PitchRollYawArray[0])
-                print(self.PitchRollYawArray[0])
-                let pitch = (self.PitchRollYawArray[0].attitude.pitch + self.PitchRollYawArray[1].attitude.pitch + self.PitchRollYawArray[2].attitude.pitch + self.PitchRollYawArray[3].attitude.pitch + self.PitchRollYawArray[4].attitude.pitch) / 5
-                let roll = (self.PitchRollYawArray[0].attitude.roll + self.PitchRollYawArray[1].attitude.roll + self.PitchRollYawArray[2].attitude.roll + self.PitchRollYawArray[3].attitude.roll + self.PitchRollYawArray[4].attitude.roll) / 5
-                let yaw = (self.PitchRollYawArray[0].attitude.yaw + self.PitchRollYawArray[1].attitude.yaw + self.PitchRollYawArray[2].attitude.yaw + self.PitchRollYawArray[3].attitude.yaw + self.PitchRollYawArray[4].attitude.yaw) / 5
-                let average = Calibration(pitch: pitch, roll: roll, yaw: yaw)
-                completion(averageCalibration: average)
-            })
-        } else {
-            completion(averageCalibration: nil)
-        }
     }
     
     func checkFlick(completion:(success: Bool) -> Void) {
