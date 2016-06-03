0f1e13e37b56d69c28d6d0ec20389ff5d7894888
Gibson Smiley <gibsonsmiley@gmail.com>
2016-06-03 16:23:57 -0600

    Send

--- iDuel/DuelController2.swift
+++ iDuel/DuelController2.swift
@@ -119,7 +119,7 @@ class DuelController2 {
     static func sendShotToDuel(duel: Duel, user: User, completion: (success: Bool) -> Void) {
         guard let userID = user.id else { completion(success: false); return }
         guard let duelID = duel.id else { completion(success: false); return }
-        FirebaseController.base.childByAppendingPath("duels/\(duelID)/shotsFired").childByAppendingPath("\(userID)").setValue("\(NSDate().timeIntervalSince1970)")
+        FirebaseController.base.childByAppendingPath("duels/\(duelID)/shotsFired").childByAppendingPath("\(NSDate().timeIntervalSince1970)").setValue("\(userID)")
         completion(success: true)
     }
     
@@ -146,21 +146,23 @@ class DuelController2 {
                                     winner = usersArray[0]
                                     loser = usersArray[1]
                                     print("First in array is winner | Winner: \(winner!.nickname) Loser: \(loser!.nickname) on Controller")
-                                    completion(winner: winner, loser: loser)
+//                                    completion(winner: winner, loser: loser)
                                 } else {
                                     winner = usersArray[0]
                                     loser = usersArray[1]
                                     print("Last in array is winner | Winner: \(winner!.nickname) Loser: \(loser!.nickname) on Controller")
-                                    completion(winner: winner, loser: loser)
+//                                    completion(winner: winner, loser: loser)
                                 }
                             } else {
                                 print("User array not at 2")
                             }
+                            completion(winner: winner, loser: loser)
                         }
                     }
                 })
             }
         }
+        
     }
     
     static func orderDuels(duels: [Duel]) -> [Duel] {
